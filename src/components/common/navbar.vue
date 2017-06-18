ef52592c78c9a44b82f69048bc6f688f5624beb5
lushijie <lushijie1218@126.com>
2017-06-18 20:53:05 +0800

    update

--- src/components/common/navbar.vue
+++ src/components/common/navbar.vue
@@ -1,22 +1,22 @@
 <template>
   <ul class="clearfix">
     <li>
-      <router-link to="/m1"><i class="fa fa-circle" aria-hidden="true"></i>初识vue</router-link>
+      <router-link to="/m1"><i class="fa fa-superpowers" aria-hidden="true"></i>初识vue</router-link>
     </li>
     <li>
-      <router-link to="/m2"><i class="fa fa-circle" aria-hidden="true"></i>路由切换</router-link>
+      <router-link to="/m2"><i class="fa fa-superpowers" aria-hidden="true"></i>路由切换</router-link>
     </li>
     <li>
-      <router-link to="/m3"><i class="fa fa-circle" aria-hidden="true"></i>内容分发</router-link>
+      <router-link to="/m3"><i class="fa fa-superpowers" aria-hidden="true"></i>内容分发</router-link>
     </li>
     <li>
-      <router-link to="/m4"><i class="fa fa-circle" aria-hidden="true"></i>事件</router-link>
+      <router-link to="/m4"><i class="fa fa-superpowers" aria-hidden="true"></i>事件</router-link>
     </li>
     <li>
-      <router-link to="/m5"><i class="fa fa-circle" aria-hidden="true"></i>slot 子组件</router-link>
+      <router-link to="/m5"><i class="fa fa-superpowers" aria-hidden="true"></i>slot 子组件</router-link>
     </li>
      <li>
-      <router-link to="/m6"><i class="fa fa-circle" aria-hidden="true"></i>render 函数</router-link>
+      <router-link to="/m6"><i class="fa fa-superpowers" aria-hidden="true"></i>render 函数</router-link>
     </li>
   </ul>
 </template>
@@ -33,5 +33,8 @@
     float: left;
     margin-right: 20px;
     font-size: 16px;
+  }
+  .fa {
+    margin-right: 3px;
   }
 </style>
